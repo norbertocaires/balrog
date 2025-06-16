8a8d2cc6c455163f61e196fd5db41e6fdb264d76
norbertocaires <beto_caires@hotmail.com>
2025-06-15 21:04:01 -0300

    Atualizando icon

--- src/App.js
+++ src/App.js
@@ -1,14 +1,7 @@
-import { useEffect } from "react";
+import React from 'react';
 import BalrogPage from './BalrogPage/BalrogPage';
 
 function App() {
-  useEffect(() => {
-    const link = document.querySelector("link[rel~='icon']");
-    if (link) {
-      link.href = `/favicon.ico?v=${new Date().getTime()}`; // força o cache a atualizar
-    }
-  }, []);
-
   return <BalrogPage />;
 }
 
